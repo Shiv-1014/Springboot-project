ce39387dd894241a064f694c1450dc85c8a40484
Shiv Mishra <shivh1007@gmail.com>
2024-02-28 11:33:17 +0530

    initial commit

---
+++ src/main/java/com/org/springboot/pkg/controller/UserinfoController.java
@@ -0,0 +1,13 @@
+package com.org.springboot.pkg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserinfoController {
+   @GetMapping("/user/{name}")
+    public String getUserName(@PathVariable String name){
+        return "Welcome to springboot " +name;
+    }
+}
